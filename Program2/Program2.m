5587db64af461e76fe39d46e9c45456a57a1d9b3
August Dixon <augustpdixon@gmail.com>
2018-11-16 16:59:21 -0600

    Finished Code

--- Program2/Program2.m
+++ Program2/Program2.m
@@ -2,7 +2,7 @@
 
 %Constants
 alpha = .12;
-numSamples = 10000;
+numSamples = 100000;
 sampleRate = 44100;
 
 %Compute expected values
@@ -27,10 +27,29 @@ c2 = essquared + em2squared;
 
 
 [V, D] = eig(R);
-xstar1 = h1' / R;
-xstar2 = h2' / R;
+fprintf('Eigenvalues of R');
+D
+fprintf('Eigenvectors of R');
+V
+xstar1 = inv(R) * h1;
+xstar2 = inv(R) * h2;
+X1 = -3:.1:3;
+X2 = -3:.1:3;
+Z1 =@(x1, x2) c1 - 2  * [x1 x2] * h1 + [x1 x2] * R * [x1; x2];
+Z2 =@(x1, x2) c2 - 2  * [x1 x2] * h2 + [x1 x2] * R * [x1; x2];
 
+lambdaMax = R(1, 1);
+if R(2,2) > lambdaMax
+    lambdaMax = R(2,2);
+end
+maxLearning = 1 / lambdaMax
 
+figure(1);
+fcontour(Z1);
+title('Contour Plot, m(k) = .12sin(2pik/3+pi/2)');
+figure(2);
+fcontour(Z2);
+title('Contour Plot, m(k) = 1.2sin(2pik/3-3pi/2)');
 
 %Array Instantiations
 W1 = [0 0];
@@ -82,13 +101,18 @@ for k = 2:numSamples
     W2 = W2 + 2 * alpha * e2(k) * [v(k) v(k-1)];
 end
 
+xstar1
+W1
+xstar2
+W2
+
 %Plot
 k = 1:100;
-figure(1);
+figure(3);
 plot(k, r1(1:100), k, s(1:100));
 title('Original and Restored, m(k) = .12sin(2pik/3+pi/2)');
 legend({'Restored','Original'}, 'Location', 'northeast');
-figure(2);
+figure(4);
 plot(k, r2(1:100), k, s(1:100));
 title('Original and Restored, m(k) = 1.2sin(2pik/3-3pi/2)');
 legend({'Restored','Original'}, 'Location', 'northeast');
@@ -97,10 +121,10 @@ legend({'Restored','Original'}, 'Location', 'northeast');
 
 
 %Write audio files
-%audiowrite('Original.mp4', s, sampleRate);
-%audiowrite('Noise1.mp4', m1, sampleRate);
-%audiowrite('Corrupted1.mp4', t1, sampleRate);
-%audiowrite('Restored1.mp4', r1, sampleRate);
-%audiowrite('Noise2.mp4', m2, sampleRate);
-%audiowrite('Corrupted2.mp4', t2, sampleRate);
-%audiowrite('Restored2.mp4', r2, sampleRate);
+audiowrite('Original.mp4', s, sampleRate);
+audiowrite('Noise1.mp4', m1, sampleRate);
+audiowrite('Corrupted1.mp4', t1, sampleRate);
+audiowrite('Restored1.mp4', r1, sampleRate);
+audiowrite('Noise2.mp4', m2, sampleRate);
+audiowrite('Corrupted2.mp4', t2, sampleRate);
+audiowrite('Restored2.mp4', r2, sampleRate);
